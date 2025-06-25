ef2983e2bbd326d3177973c78ae0305525f86144
TakashufShakoor <takashufshakoor1@gmail.com>
2025-06-25 09:53:00 +0500

    CORS error Fix

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json())
 //app.use(cors())
 
 app.use(cors({
-  origin: 'https://doc-hub-sigma-umber.vercel.app/',
+  origin: 'https://doc-hub-sigma-umber.vercel.app',
   credentials: true
 }));
 
